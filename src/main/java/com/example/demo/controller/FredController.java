3f4b41b855877e9e27632c39a17716ecd0a4a2aa
roehd <mekieun@ewhain.net>
2021-08-07 13:30:08 +0900

    week 2

---
+++ src/main/java/com/example/demo/controller/FredController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import java.time.Duration;
+
+@RestController
+@RequestMapping("/fred")
+@Slf4j
+public class FredController {
+    @GetMapping(path="/interval", produces  = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<Long> interval(){
+        return Flux.interval(Duration.ofMillis(300));
+    }
+
+}
